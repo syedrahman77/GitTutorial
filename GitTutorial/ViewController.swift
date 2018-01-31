6bda4e264418f2f5bee7502588cbdada18d99758
Syed Rahman <syed.rahman@gmail.com>
2018-01-30 20:44:02 -0500

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Syed Rahman on 2018-01-30.
+//  Copyright © 2018 Mountain & Fox Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
